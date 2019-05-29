f73b93da46e376fb225dd4b00913b9408bcc5e34
rishi <rishirsud@gmail.com>
2019-05-29 10:26:45 -0400

    Updates

---
+++ precache-manifest.f5e0b199e3080fc6e16eb0fca056db3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2806d1022dd12bacfa50205a57130e4",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "79f2df5e1ee0b52df9c5",
+    "url": "/ClickyGame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/ClickyGame/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "79f2df5e1ee0b52df9c5",
+    "url": "/ClickyGame/static/js/main.132455ab.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
